23da895485c9669559a0aeb99194d24fe2f17688
hall-e3 <hallenochasanda@gmail.com>
2024-02-13 11:44:45 +0300

    github workflow changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  // Other Next.js configurations...
-
-  // Add the following export configuration
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' },
-      // Add other pages as needed
-    };
-  },
+const nextConfig = {
+  output: "export"
 };
+
+export default nextConfig;
