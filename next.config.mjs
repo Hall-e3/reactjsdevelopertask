9b442aeec591451ec9b2b05a218fccbfd8b2e891
hall-e3 <hallenochasanda@gmail.com>
2024-02-13 18:04:29 +0300

    github workflow changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
-/**
- * @type {import('next').NextConfig}
- */
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   basePath: isProd ? "/reactjsdevelopertask" : "",
   output: "export",
-  distDir: "dist",
+  distDir: "dist", // Update this line
   images: {
     unoptimized: true
   }
