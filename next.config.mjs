a5ecb9ed59d796b14c0a646f03650929e4e9de93
hall-e3 <hallenochasanda@gmail.com>
2024-02-13 16:27:30 +0300

    github workflow changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  experimental: {
+    images: false // Assuming you don't need the experimental images feature
+  },
   exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
     return {
       "/": { page: "/" }
-      // Add other pages as needed
     };
   }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
