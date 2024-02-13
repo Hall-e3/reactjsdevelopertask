3f55935069644dd2ef0b0b2c8938853edc001d32
hall-e3 <hallenochasanda@gmail.com>
2024-02-13 14:12:39 +0300

    github workflow changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
     return {
       "/": { page: "/" }
+      // Add other pages as needed
     };
   }
 };
