36294ae33c7d58c339720aa46645b889487ae6e1
hall-e3 <hallenochasanda@gmail.com>
2024-02-13 11:14:17 +0300

    github workflow changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
-module.exports = {
-  // other configurations...
-  target: "experimental-serverless-trace",
-  exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
-    // Add any custom path mappings if needed
-    return {
-      "/": { page: "/" }
-      // Add other paths as needed
-    };
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export"
 };
+
+export default nextConfig;
